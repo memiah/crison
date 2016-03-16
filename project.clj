6bdc5b9d55ea96451737ee21e45c272985a05698
rossputin <rossajmcd@gmail.com>
2016-03-16 00:01:22 +0000

    fix formatting in project.clj

--- project.clj
+++ project.clj
@@ -5,13 +5,13 @@
                  [environ "1.0.2"]
                  [clj-webdriver "0.7.2-SNAPSHOT"]
                  [org.seleniumhq.selenium/selenium-server "2.47.1"]
-                                  ;; Needed by core code
-                                  [org.seleniumhq.selenium/selenium-java "2.47.0"]
-                                  [org.seleniumhq.selenium/selenium-remote-driver "2.47.1"]
-                                  [com.codeborne/phantomjsdriver "1.2.1"
-                                   :exclusion [org.seleniumhq.selenium/selenium-java
-                                               org.seleniumhq.selenium/selenium-server
-                                               org.seleniumhq.selenium/selenium-remote-driver]]]
+                 ;; Needed by core code
+                 [org.seleniumhq.selenium/selenium-java "2.47.0"]
+                 [org.seleniumhq.selenium/selenium-remote-driver "2.47.1"]
+                 [com.codeborne/phantomjsdriver "1.2.1"
+                   :exclusion [org.seleniumhq.selenium/selenium-java
+                               org.seleniumhq.selenium/selenium-server
+                               org.seleniumhq.selenium/selenium-remote-driver]]]
 
   :aot :all
   :main crison.main)
