3e0b36732514c1300b911c23a500e023132ca039
rossputin <rossajmcd@gmail.com>
2016-04-02 15:53:25 +0100

    bump version to 0.2.0-SNAPSHOT (there is still a problem with a dependency

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject crison "0.1.0-SNAPSHOT"
+(defproject crison "0.2.0-SNAPSHOT"
   :description "A data language for driving websites"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
